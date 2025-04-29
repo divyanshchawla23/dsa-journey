63c770628face28c164838df6d0416fb1774a8c4
Divyansh Chawla <divyanshchawla23@gmail.com>
2025-04-29 17:36:11 +0530

    April 29

--- Step 2 - Learn Sorting Techniques/QuickSort.cpp
+++ Step 2 - Learn Sorting Techniques/QuickSort.cpp
@@ -1,21 +1,45 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 // PICK PIVOT AND PLACE IT IN CORRECT POSITION
 // TIME COMPLEXITY =  O(NLogN)  WORST, AVERAGE and BEST CASE
-// SPACE COMPLEXITY =  O(1)      
+// SPACE COMPLEXITY =  O(1)
 
-int findPivot(vector<int> &arr, int low , int high){
+int findPivot(int arr[], int low, int high)
+{
+  int pivot = arr[low];
+  int i = low;
+  int j = high;
+  while (i < j)
+  {
+    while (i <= high - 1 && arr[i] <= pivot)
+    {
+      i++;
+    }
 
+    while (j >= low - 1 && arr[j] > pivot)
+    {
+      j--;
+    }
 
-
+    if (i < j)
+    {
+      swap(arr[i], arr[j]);
+    }
+  }
+  swap(arr[low], arr[j]);
+  return j;
 }
 
-void quick_sort(vector<int> &arr, int low , int high) {
+void quick_sort(int arr[], int low, int high)
+{
 
-  
-
-
+  if (low < high)
+  {
+    int pIndex = findPivot(arr, low, high);
+    quick_sort(arr, low, pIndex - 1);
+    quick_sort(arr, pIndex + 1, high);
+  }
 }
 
 int main()
@@ -29,14 +53,15 @@ int main()
     cout << arr[i] << " ";
   }
   cout << "\n";
-  
-  quick_sort(arr, n , 0, n-1);
+
+  quick_sort(arr, 0, n - 1);
 
   cout << "After quick sort: " << "\n";
   for (int i = 0; i < n; i++)
   {
     cout << arr[i] << " ";
   }
-  
+  cout << "\n";
+
   return 0;
 }
