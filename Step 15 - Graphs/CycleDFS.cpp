ef820bc8be772f963827d976bea7ba780c56cb82
Divyansh Chawla <divyanshchawla23@gmail.com>
2025-05-01 17:23:45 +0530

    May 1

---
+++ Step 15 - Graphs/CycleDFS.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool detect(int node, int parent, vector<vector<int>> &adj, vector<int> &vis)
+{
+
+    vis[node] = 1;
+
+    for (auto it : adj[node])
+    {
+        if (!vis[it])
+        {
+            if (detect(it, node, adj, vis))
+                return true;
+        }
+        else if (it != parent)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    int n = 4; // number of nodes
+    vector<vector<int>> adj(n + 1);
+
+    // Edges: (1–2), (2–3), (3–1) => cycle
+    // Extra edge: (3–4)
+    adj[1].push_back(2);
+    adj[2].push_back(1);
+
+    adj[2].push_back(3);
+    adj[3].push_back(2);
+
+    adj[3].push_back(1);
+    adj[1].push_back(3);
+
+    adj[3].push_back(4);
+    adj[4].push_back(3);
+
+    vector<int> ans;
+    vector<int> vis(n + 1, 0);
+    bool flag = false;
+
+    for (int i = 1; i < n; i++)
+    {
+        flag = detect(i, -1, adj, vis);
+    }
+
+    if (flag)
+        cout << "True" << endl;
+    else
+        cout << "False" << endl;
+
+    return 0;
+}
