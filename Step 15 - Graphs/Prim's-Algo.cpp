5f0f55df587e590f0c25e63a7e5e193cc8c52ac0
Divyansh Chawla <divyanshchawla23@gmail.com>
2025-05-19 21:11:44 +0530

    May 19

--- Step 15 - Graphs/Prim's-Algo.cpp
+++ Step 15 - Graphs/Prim's-Algo.cpp
@@ -1,42 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-  public:
-    // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[]) {
-        
-        vector<int> vis(V,0);
+class Solution
+{
+public:
+    
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+
+        vector<int> vis(V, 0);
 
         //{{wt,node},parent};
-        priority_queue<pair<pair<int,int>,int>, 
-        vector<pair<pair<int,int>,int>>,
-        greater<pair<pair<int,int>,int>>> pq;
-        
-        pq.push({{0,0},-1});
-        
+        priority_queue<pair<pair<int, int>, int>,
+                       vector<pair<pair<int, int>, int>>,
+                       greater<pair<pair<int, int>, int>>> pq;
+
+        pq.push({{0, 0}, -1});
+
         int ans = 0;
-        
-        while(!pq.empty()){
+
+        while (!pq.empty())
+        {
             int node = pq.top().first.second;
             int wt = pq.top().first.first;
             int parent = pq.top().second;
             pq.pop();
-            
-            if(vis[node]) continue;             // if it is visited, it is the part of our MST, so continue;
-            
-            vis[node]= 1;
-            if(parent!= -1) v.push_back({{nd,parent},wt});      // this line is for storing the tree edges 
-            ans+= wt;
-            
-            for(auto it: adj[node]){
-                
-                if(!vis[it[0]]){
-                    pq.push({{it[1],it[0]},node});
+
+            if (vis[node])
+                continue; // if it is visited, it is the part of our MST, so continue;
+
+            vis[node] = 1;
+            if (parent != -1)
+                v.push_back({{nd, parent}, wt}); // this line is for storing the tree edges
+            ans += wt;
+
+            for (auto it : adj[node])
+            {
+
+                if (!vis[it[0]])
+                {
+                    pq.push({{it[1], it[0]}, node});
                 }
             }
         }
-        
+
         return ans;
     }
 };
